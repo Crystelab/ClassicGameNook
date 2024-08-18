28b6c766ce199e8adfbea8420e84cc96f000ff3f
Crystel2002 <crystelabounahed@hotmail.com>
2024-08-18 14:27:41 -0400

    Dropdown

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,27 @@
+var btnTitle = "Theme";
+
 function toggleDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
+    const arrow = document.querySelector(".dropbtn .arrow");
+    dropdown.classList.toggle("show");
+    if (dropdown.classList.contains("show")) {
+        arrow.classList.remove("down");
+        arrow.classList.add("up");
+    } else {
+        arrow.classList.remove("up");
+        arrow.classList.add("down");
+    }
 }
 
+/*document.addEventListener("DOMContentLoaded", function() {
+    // Set the button's text content to the value of btnTitle
+    var dropdownButton = document.querySelector('.dropbtn');
+    if (dropdownButton) {
+        dropdownButton.textContent = btnTitle;
+    }
+});
+*/
+// Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -12,4 +32,4 @@ window.onclick = function(event) {
             }
         }
     }
-}
+};
