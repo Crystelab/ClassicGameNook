1760245d944b8b91b2f88ecf93c34107e1285b67
Crystel2002 <crystelabounahed@hotmail.com>
2024-08-19 11:26:35 -0400

    Thumbnail

--- js/script.js
+++ js/script.js
@@ -1,22 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function loadComponent(elementId, componentPath) {
-        fetch(componentPath)
-            .then(response => response.text())
-            .then(data => {
-                const element = document.getElementById(elementId);
-                if (element) {
-                    element.innerHTML = data;
-                    console.log(`Loaded content into #${elementId}`);
-                } else {
-                    console.error(`Element with ID ${elementId} not found.`);
-                }
-            })
-            .catch(error => console.error(`Failed to load component from ${componentPath}:`, error));
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  function loadComponent(elementId, componentPath) {
+    fetch(componentPath)
+      .then((response) => response.text())
+      .then((data) => {
+        const element = document.getElementById(elementId);
+        if (element) {
+          element.innerHTML = data;
+          console.log(`Loaded content into #${elementId}`);
+        } else {
+          console.error(`Element with ID ${elementId} not found.`);
+        }
+      })
+      .catch((error) =>
+        console.error(`Failed to load component from ${componentPath}:`, error)
+      );
+  }
 
-    // Load components
+  //Tic-Tac-Toe assets
+  document.querySelectorAll(".o-container").forEach((container) => {
+    fetch("assets/tictactoe/o.html")
+      .then((response) => response.text())
+      .then((html) => {
+        container.innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading the SVG:", error);
+      });
+  });
 
-    loadComponent('navbar', 'html/components/navbar.html');
-    loadComponent('separator', 'html/components/separator.html');
-    loadComponent('logo-container', 'assets/logo.html'); 
+  document.querySelectorAll(".x-container").forEach((container) => {
+    fetch("assets/tictactoe/x.html")
+      .then((response) => response.text())
+      .then((html) => {
+        container.innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading the SVG:", error);
+      });
+  });
+
+  document.querySelectorAll(".grid-container").forEach((container) => {
+    fetch("assets/tictactoe/grid.html")
+      .then((response) => response.text())
+      .then((html) => {
+        container.innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading grid:", error);
+      });
+  });
+  
+  
+
+  // Load components
+  loadComponent("navbar", "html/components/navbar.html");
+  loadComponent("separator", "html/components/separator.html");
+  loadComponent("logo-container", "assets/logo.html");
 });
